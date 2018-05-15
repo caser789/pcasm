d74d09ef03a7b8fa099d61c80a64a5fd0897cffb
jiao.xue <jiao.xue@ihandysoft.com>
2018-05-15 21:51:30 +0800

    math.square

--- math.asm
+++ math.asm
@@ -2,6 +2,7 @@
 
 segment .data
 prompt db "Enter a number: ", 0
+square_msg db "Square of input is ", 0
 
 segment .bss
 input resd 1
@@ -12,9 +13,17 @@ segment .text
 
 asm_main:
     enter 0,0
-    push a
+    pusha
 
     mov eax, prompt
+    call print_string
+
+    call read_int
+    mov [input], eax
+
+    imul eax
+    mov ebx, eax
+    mov eax, square_msg
     call print_string
 
     popa
