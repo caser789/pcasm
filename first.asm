55ce0f1953f9795d99aa57d4cb2ea1b3df72daef
jiao.xue <jiao.xue@ihandysoft.com>
2018-05-15 16:22:04 +0800

    whole first program

--- first.asm
+++ first.asm
@@ -17,6 +17,8 @@ segment .data
 prompt1 db "Enter a number: ", 0
 prompt2 db "Enter another number: ", 0
 outmsg1 db "You entered ", 0
+outmsg2 db " and ", 0
+outmsg3 db ", the sum of these is ", 0
 ;
 ; uninitialized data is put in the .bss segment
 ;
@@ -53,6 +55,25 @@ asm_main:
 
     dump_regs 1
     dump_mem 2, outmsg1, 1
+
+    mov eax, outmsg1
+    call print_string
+
+    mov eax, [input1]
+    call print_int
+
+    mov eax, outmsg2
+    call print_string
+
+    mov eax, [input2]
+    call print_int
+
+    mov eax, outmsg3
+    call print_string
+
+    mov eax, ebx
+    call print_int
+    call print_nl
 
     popa
     mov eax, 0
